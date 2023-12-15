b9ea865fbe68279b826676a22227d4b8438ab59a
David Barton <barton.david.sv@gmail.com>
2023-12-15 22:01:07 +0100

    feat: introduce playwright and multimodal scraping

--- app.py
+++ app.py
@@ -1,17 +1,39 @@
-import re
-import urllib.request
-from bs4 import BeautifulSoup
+from playwright.sync_api import sync_playwright
 
-with urllib.request.urlopen('https://www.hazipatika.com/orvosmeteorologia') as response:
-    html = response.read()
-soup = BeautifulSoup(html, 'html.parser')
-front_name = soup.css.select('.o-humanMeteoDay__frontEffectBox span')[0].text.lower()
+# Function to scrape text content from a web page using Playwright
+def scrape(url, selector):
+    # Launch a Playwright instance and perform scraping
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        page.goto(url)
+        text_raw = page.locator(selector)
+        text = text_raw.first.text_content().lower()
+        browser.close()
+        return text
 
-if 'hidegfront' in front_name:
-    print('hidegfront 🥶')
-elif 'melegfront' in front_name:
-    print('melegfront 🥵')
-elif 'kettősfront' in front_name:
-    print('kettősfront 🤕')
-else:
-    print('nincs front 😊')
+# Function to identify weather front based on extracted text
+def front_searcher(front_name):
+    if 'hidegfront' in front_name:
+        return 'hidegfront 🥶'
+    elif 'melegfront' in front_name:
+        return 'melegfront 🥵'
+    elif 'kettősfront' in front_name:
+        return 'kettősfront 🤕'
+    else:
+        return 'nincs front 😊'
+
+# Scrape weather front information from two different URLs
+front_name1 = scrape(
+    'https://www.hazipatika.com/orvosmeteorologia',
+    '.o-humanMeteoDay__frontEffectBox span')
+front_name2 = scrape(
+    'https://www.idokep.hu/elorejelzes/Budapest', 
+    '.forecast-box-text')
+
+# Determine weather fronts using the front_searcher function
+f1 = front_searcher(front_name1)
+f2 = front_searcher(front_name2)
+
+# Output the identified weather fronts
+print(f1 + ' - ' + f2)
